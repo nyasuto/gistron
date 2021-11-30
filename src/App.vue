861f6be968e2f9ca479d84d0361b2168c3feee9f
yast <yast@me.com>
2021-11-30 21:53:21 +0900

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <EditorMain msg="hoge"/>
+  <div class="container">
+    <header class="header">
+      header <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    </header>
+    <aside class="aside">
+      <EditorMain msg="hoge"/>
+    </aside>
+  </div>
 </template>
 
 <script>
@@ -15,6 +21,57 @@ export default {
 </script>
 
 <style>
+.container {
+  display: grid;
+
+  grid-template-columns: 180px 1fr 160px;
+
+  grid-template-rows: 60px 1fr 90px;
+}
+
+.header {
+  /* 列の1本目から4本目のグリッドラインまで */
+  grid-column: 1 / 4;
+  /* 行の1本目から2本目のグリッドラインまで */
+  grid-row: 1;
+}
+
+.main {
+  /* 列の2本目から3本目のグリッドラインまで */
+  grid-column-start: 2;
+  grid-column-end: auto;
+  /* 行の2本目から3本目のグリッドラインまで */
+  grid-row-start: 2;
+  grid-row-end: auto;
+}
+
+.aside {
+  /* 列の1本目から2本目のグリッドラインまで */
+  grid-column-start: 1;
+  grid-column-end: auto;
+  /* 行の2本目から3本目のグリッドラインまで */
+  grid-row-start: 2;
+  grid-row-end: auto;
+}
+
+.nav {
+  /* 列の3本目から4本目のグリッドラインまで */
+  grid-column-start: 3;
+  grid-column-end: auto;
+  /* 行の2本目から3本目のグリッドラインまで */
+  grid-row-start: 2;
+  grid-row-end: auto;
+}
+
+.footer {
+  /* 列の1本目から4本目のグリッドラインまで */
+  grid-column-start: 1;
+  grid-column-end: 4;
+  /* 行の3本目から4本目のグリッドラインまで */
+  grid-row-start: 3;
+  grid-row-end: auto;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/EditorMain.vue
+++ src/components/EditorMain.vue
@@ -2,7 +2,7 @@
   <div class="editor">
     <ul id="gist-list">
       <li v-for="gist in data.gists" :key="gist.url">
-        {{gist.description }} - {{ gist.url}}
+        {{ gist.description }} <!-- - {{ gist.url }} -->
       </li>
     </ul>
 
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import { Octokit } from "@octokit/core";
+import {Octokit} from "@octokit/core";
 
 export default {
   name: "EditorMain",
@@ -27,12 +27,12 @@ export default {
       auth: process.env.VUE_APP_GITHUB_TOKEN,
     });
     octokit.request("/gists")
-    .then(
-      (data) => {
-        console.log(data)
-        this.data.gists = data.data
-        }
-      );
+        .then(
+            (data) => {
+              // console.log(data)
+              this.data.gists = data.data
+            }
+        );
   },
 };
 </script>
@@ -42,14 +42,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
